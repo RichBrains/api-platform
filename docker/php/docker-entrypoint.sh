aa3a1d1e22cfd26c464ffa928b583c86a339d4ed
Evgeny Marchenkov <e.marchenkov@richbrains.net>
2021-02-10 18:15:16 +0300

    fixed issue

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -43,9 +43,10 @@ fi
 			echo "The database is now ready and reachable"
 		fi
 
-		if ls -A migrations/*.php >/dev/null 2>&1; then
-			php bin/console doctrine:migrations:migrate --no-interaction
-		fi
+                # if necessary
+		#if ls -A migrations/*.php >/dev/null 2>&1; then
+		#	php bin/console doctrine:migrations:migrate --no-interaction
+		#fi
 	fi
 #fi
 
